f101a4d68cc16fc15888c30671fbd60d3a7f4173
« Ephely » <ephely1@gmail.com>
2025-03-22 00:02:19 +0100

    Initial commit

---
+++ part3/config.py
@@ -0,0 +1,15 @@
+import os
+
+class Config:
+    SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "another-secret-key")
+    SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
+    DEBUG = False
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+config = {
+    'development': DevelopmentConfig,
+    'default': DevelopmentConfig
+}
